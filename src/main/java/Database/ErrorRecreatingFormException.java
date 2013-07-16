b96dd8fb36ad8c928fc81f3ea9e81dddb2dd1dbf
Andrew Hanes <ahanes@csh.rit.edu>
2013-07-15 20:24:27 -0400

    Exceptions for the database

---
+++ src/main/java/Database/ErrorRecreatingFormException.java
@@ -0,0 +1,12 @@
+package Database;
+
+/**
+ * Author: Andrew Hanes
+ * Date: 7/15/13
+ * Time: 7:27 PM
+ */
+public class ErrorRecreatingFormException extends Exception {
+    public ErrorRecreatingFormException(String s) {
+        super(s);
+    }
+}
