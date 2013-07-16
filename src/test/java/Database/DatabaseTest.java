7b71a8f1b5c425d5e899bc9cfba5d3b6a41cb561
Andrew Hanes <ahanes@csh.rit.edu>
2013-07-15 20:52:20 -0400

    See if travisci works.

--- src/test/java/Database/DatabaseTest.java
+++ src/test/java/Database/DatabaseTest.java
@@ -1,71 +1,62 @@
 package Database;
 
-import fields.CheckBoxField;
-import fields.FieldHeader;
-import fields.Form;
-import fields.LongTextField;
+import junit.framework.TestCase;
 /**
  * Author: Andrew Hanes
- * Date: 7/14/13
- * Time: 6:08 PM
+ * Date: 7/15/13
+ * Time: 8:48 PM
  */
-public class DatabaseTest {
+public class DatabaseTest extends TestCase {
+    Database d;
+    public void setUp() throws Exception {
+        d = new Database("localhost", "postgres", "", "myapp_test"); //For travis-ci
+    }
 
-  //  @Test
     public void testAddVotingUser() throws Exception {
-        new Database().addVotingUser(1);
-    }
-  //  @Test
-    public void testVotingUserExists() throws Exception {
-        System.out.println("ASD");
-        assert(new Database().globalVotingUserExists(1));
-        assert(!new Database().globalVotingUserExists(-1));
+        System.out.println("Setting up");
     }
 
-  //  @Test
-    public void testCreateObject() throws Exception {
-        Database d = new Database();
+    public void testGlobalVotingUserExists() throws Exception {
+
     }
 
-  //  @Test
+    public void testAddFormVotingUser() throws Exception {
+
+    }
+
+    public void testRemoveFormVotingUser() throws Exception {
+
+    }
+
+    public void testIsValidFormVoter() throws Exception {
+
+    }
+
     public void testStoreForm() throws Exception {
-        Database d = new Database();
-        Form f = new Form();
-        f.addField(new LongTextField(50));
-        f.addField(new CheckBoxField());
-        f.addField(new FieldHeader("Hello World"));
-        d.storeForm(1, f);
+
     }
 
-   // @Test
-    public void testGetAllUsersFormIds() throws Exception {
-        Database d = new Database();
-        d.getAllUserFormIds(1);
+    public void testGetAllUserFormIds() throws Exception {
+
     }
 
-   // @Test
     public void testGetForm() throws Exception {
-        Database d = new Database();
-        Form f = d.getForm(1);
-        assert(f.getField(2) instanceof FieldHeader);
-        FieldHeader head= (FieldHeader) f.getField(2);
-        System.out.println(head.getText());
-    }
-    //@Test
-    public void testAddGlobalUser() throws Exception {
-        Database d = new Database();
-        d.addVotingUser(93123);
+
     }
 
-    //@Test
-    public void testRemoveGlobalUser() throws Exception {
-        Database d = new Database();
-        assert(!d.isValidFormVoter(3, 5));
+    public void testAlreadyVoted() throws Exception {
+
     }
 
-    //@Test
-    public void testAddVotingUserToForm() throws Exception {
-        Database d = new Database();
-        d.addVotingUser(2);
+    public void testStoreFormResponse() throws Exception {
+
+    }
+
+    public void testFormExists() throws Exception {
+
+    }
+
+    public void testGetAllFormResponse() throws Exception {
+
     }
 }
